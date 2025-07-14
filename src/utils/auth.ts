44c34734b80d061d7e0889feb92deb458d526dcb
Miguel <miguel02urena@gmail.com>
2025-07-14 08:10:43 -0600

    add checkPassword: se creo una función para comparar las contraseñas en el login

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt';
 
+// Hashear password
 export const hassPassword = async (pswd : string) => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(pswd, salt);   
+}
+
+// Comprobar password
+export const checkPassword = async (pswd : string, hash : string) => {
+    return await bcrypt.compare(pswd, hash);
 }
